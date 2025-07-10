4d937335c5a648e10bbd8bb11492f216f79c663a
Mohammed Ismail Vajeehullah <vajeehvjk@gmail.com>
2025-07-10 21:56:37 +0530

    Commit 06: React Learning - Render List (3 - Using PROPS)

--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -1,4 +1,4 @@
-
+import PropTypes from 'prop-types';
 
 function List(props) {
 
@@ -14,6 +14,18 @@ function List(props) {
             <ol className="list-item">{listItem}</ol>
         </>
     );
+}
+List.propTypes = {
+    category: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.shape({  id: PropTypes.number,
+                                                name: PropTypes.string,
+                                                calories: PropTypes.number
+                                            })),
+}
+
+List.defaultProps = {
+    category: "Category",
+    items: [],
 }
 
 export default List
